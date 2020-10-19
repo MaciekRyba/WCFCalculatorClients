ec8c6b95aa46bffce6db2f9f4f2f9322d277c9a7
MaciekRyba <maciekryba15@gmail.com>
2020-10-19 19:21:28 +0200

    Okienkowy i konsolwy klient

---
+++ ConsoleComplexCalculator/ConsoleComplexCalculator/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using ComplexCalculatorService;
+
+namespace ConsoleComplexCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sOption ="";
+
+            do
+            {
+
+                double firsRealValue = 0; double secondRealValue = 0; double firstImaginaryValue = 0; double secondImaginaryValue = 0;
+                var ComplexCalc = new ComplexServicesClient();
+                var FirstComplex = new ComplexType();
+                var SecondComplex = new ComplexType();
+                string result = "";
+
+                Console.WriteLine("Console Complex Calculator\r");
+                Console.WriteLine("------------------------\n");
+
+                Console.WriteLine("Proszę wybrać opcje z listy:\n");
+                Console.WriteLine("\ta - Add");
+                Console.WriteLine("\ts - Subtract");
+                Console.WriteLine("\tm - Multiply");
+                Console.WriteLine("\td - Divide");
+                Console.WriteLine("\taC - Complex(Add)");
+                Console.WriteLine("\tsC - Complex(Subtract)");
+                Console.WriteLine("\tmC - Complex(Multiply)");
+                Console.WriteLine("\tdC - Complex(Divide)");
+                Console.WriteLine("\tc - czyść console");
+                Console.WriteLine("\te - zakończ");
+                sOption = Console.ReadLine();
+
+                if (sOption != "c" && sOption != "e")
+                {
+                    Console.WriteLine("Wpisz pierwszą liczbę rzeczywista");
+                    firsRealValue = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Wpisz druga liczbę rzeczywista");
+                    secondRealValue = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Wpisz pierwszą liczbę urojona");
+                    firstImaginaryValue = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Wpisz druga liczbę urojona");
+                    secondImaginaryValue = Convert.ToInt32(Console.ReadLine());
+                }
+
+                switch (sOption)
+                {
+                    case "a":
+                        result = ComplexCalc.AddFourValueAsync(firsRealValue,secondRealValue,firstImaginaryValue,secondImaginaryValue).Result;
+                        Console.WriteLine(result);
+                        break;
+                    case "s":
+                        result = ComplexCalc.SubtractFourValue(firsRealValue, secondRealValue, firstImaginaryValue, secondImaginaryValue);
+                        Console.WriteLine(result);
+                        break;
+                    case "m":
+                        result = ComplexCalc.MultiplyFourValue(firsRealValue, secondRealValue, firstImaginaryValue, secondImaginaryValue);
+                        Console.WriteLine(result);
+                        break;
+                    case "d":
+                        result = ComplexCalc.DivisionFourValue(firsRealValue, secondRealValue, firstImaginaryValue, secondImaginaryValue);
+                        Console.WriteLine(result);
+                        break;
+                    case "aC":
+                        FirstComplex.RealValueOperation = firsRealValue;
+                        FirstComplex.ImaginryValueOperation = firstImaginaryValue;
+                        SecondComplex.RealValueOperation = secondRealValue;
+                        SecondComplex.ImaginryValueOperation = secondImaginaryValue;
+                        result = ComplexCalc.AddTwoValue(FirstComplex,SecondComplex);
+                        Console.WriteLine(result);
+                        break;
+                    case "sC":
+                        FirstComplex.RealValueOperation = firsRealValue;
+                        FirstComplex.ImaginryValueOperation = firstImaginaryValue;
+                        SecondComplex.RealValueOperation = secondRealValue;
+                        SecondComplex.ImaginryValueOperation = secondImaginaryValue;
+                        result = ComplexCalc.SubtractTwoValue(FirstComplex, SecondComplex);
+                        Console.WriteLine(result);
+                        break;
+                    case "mC":
+                        FirstComplex.RealValueOperation = firsRealValue;
+                        FirstComplex.ImaginryValueOperation = firstImaginaryValue;
+                        SecondComplex.RealValueOperation = secondRealValue;
+                        SecondComplex.ImaginryValueOperation = secondImaginaryValue;
+                        result = ComplexCalc.MultiplyTwoValue(FirstComplex, SecondComplex);
+                        Console.WriteLine(result);
+                        break;
+                    case "dC":
+                        FirstComplex.RealValueOperation = firsRealValue;
+                        FirstComplex.ImaginryValueOperation = firstImaginaryValue;
+                        SecondComplex.RealValueOperation = secondRealValue;
+                        SecondComplex.ImaginryValueOperation = secondImaginaryValue;
+                        result = ComplexCalc.DivisionTwoValue(FirstComplex, SecondComplex);
+                        Console.WriteLine(result);
+                        break;
+                    case "c":
+                        Console.Clear();
+                        break;
+                }
+
+
+            } while (sOption != "e");
+        }
+    }
+}
---
+++ WpfComplexCalculator/WpfComplexCalculator/MainWindow.xaml.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ComplexCalculatorService;
+using System.ComponentModel;
+
+namespace WpfComplexCalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void bAddDouble_Click(object sender, RoutedEventArgs e)
+        {
+            var ComplexCalc = new ComplexServicesClient();
+
+            lResult.Content = ComplexCalc.AddFourValueAsync(double.Parse(tFirstReal.Text), 
+                double.Parse(tSecondReal.Text), double.Parse(tFirstImaginary.Text), double.Parse(tSecondImaginary.Text)).Result;
+        }
+
+        private void bSubDouble_Click(object sender, RoutedEventArgs e)
+        {
+            var ComplexCalc = new ComplexServicesClient();
+
+            lResult.Content = ComplexCalc.SubtractFourValue(double.Parse(tFirstReal.Text),
+                double.Parse(tSecondReal.Text), double.Parse(tFirstImaginary.Text), double.Parse(tSecondImaginary.Text));
+        }
+
+        private void bMultipyDouble_Click(object sender, RoutedEventArgs e)
+        {
+            var ComplexCalc = new ComplexServicesClient();
+
+            lResult.Content = ComplexCalc.MultiplyFourValue(double.Parse(tFirstReal.Text),
+                double.Parse(tSecondReal.Text), double.Parse(tFirstImaginary.Text), double.Parse(tSecondImaginary.Text));
+        }
+
+        private void bDivDouble_Click(object sender, RoutedEventArgs e)
+        {
+            var ComplexCalc = new ComplexServicesClient();
+
+            lResult.Content = ComplexCalc.DivisionFourValue(double.Parse(tFirstReal.Text),
+                double.Parse(tSecondReal.Text), double.Parse(tFirstImaginary.Text), double.Parse(tSecondImaginary.Text));
+        }
+
+        private void bAddComplex_Click(object sender, RoutedEventArgs e)
+        {
+            var ComplexCalc = new ComplexServicesClient();
+            var FirstComplex = new ComplexType();
+            var SecondComplex = new ComplexType();
+
+            FirstComplex.RealValueOperation = double.Parse(tFirstReal.Text);
+            FirstComplex.ImaginryValueOperation = double.Parse(tFirstImaginary.Text);
+
+            SecondComplex.RealValueOperation = double.Parse(tSecondReal.Text);
+            SecondComplex.ImaginryValueOperation = double.Parse(tSecondImaginary.Text);
+
+
+            lResult.Content = ComplexCalc.AddTwoValue(FirstComplex,SecondComplex);
+        }
+
+        private void bSubComplex_Click(object sender, RoutedEventArgs e)
+        {
+            var ComplexCalc = new ComplexServicesClient();
+            var FirstComplex = new ComplexType();
+            var SecondComplex = new ComplexType();
+
+            FirstComplex.RealValueOperation = double.Parse(tFirstReal.Text);
+            FirstComplex.ImaginryValueOperation = double.Parse(tFirstImaginary.Text);
+
+            SecondComplex.RealValueOperation = double.Parse(tSecondReal.Text);
+            SecondComplex.ImaginryValueOperation = double.Parse(tSecondImaginary.Text);
+
+
+            lResult.Content = ComplexCalc.SubtractTwoValue(FirstComplex, SecondComplex);
+        }
+
+        private void bMultipyComplex_Click(object sender, RoutedEventArgs e)
+        {
+            var ComplexCalc = new ComplexServicesClient();
+            var FirstComplex = new ComplexType();
+            var SecondComplex = new ComplexType();
+
+            FirstComplex.RealValueOperation = double.Parse(tFirstReal.Text);
+            FirstComplex.ImaginryValueOperation = double.Parse(tFirstImaginary.Text);
+
+            SecondComplex.RealValueOperation = double.Parse(tSecondReal.Text);
+            SecondComplex.ImaginryValueOperation = double.Parse(tSecondImaginary.Text);
+
+
+            lResult.Content = ComplexCalc.MultiplyTwoValue(FirstComplex, SecondComplex);
+        }
+
+        private void bDivComplex_Click(object sender, RoutedEventArgs e)
+        {
+            var ComplexCalc = new ComplexServicesClient();
+            var FirstComplex = new ComplexType();
+            var SecondComplex = new ComplexType();
+
+            FirstComplex.RealValueOperation = double.Parse(tFirstReal.Text);
+            FirstComplex.ImaginryValueOperation = double.Parse(tFirstImaginary.Text);
+
+            SecondComplex.RealValueOperation = double.Parse(tSecondReal.Text);
+            SecondComplex.ImaginryValueOperation = double.Parse(tSecondImaginary.Text);
+
+
+            lResult.Content = ComplexCalc.DivisionTwoValue(FirstComplex, SecondComplex);
+        }
+    }
+}
